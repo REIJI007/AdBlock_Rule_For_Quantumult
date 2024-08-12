96cbbcb07843f8348dce090be03d3ecdf3814e2c
REIJI007 <miyajimareji007@gmail.com>
2024-08-12 20:39:10 +0800

    Update adblock_rule_generator_quantumult_list.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_quantumult_list.ps1
+++ adblock_rule_generator_quantumult_list.ps1
@@ -93,7 +93,7 @@ $($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
-$outputPath = "$PSScriptRoot/adblock_reject_quantumult_ruleset.list"
+$outputPath = "$PSScriptRoot/adblock_reject_quantumult.list"
 $textContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数
