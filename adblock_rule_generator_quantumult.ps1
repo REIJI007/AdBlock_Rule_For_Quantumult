cf86009c601bfa2476dedfec769ec89d0ed05b7f
REIJI007 <miyajimareji007@gmail.com>
2024-10-03 02:54:39 +0800

    Update adblock_rule_generator_quantumult.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_quantumult.ps1
+++ adblock_rule_generator_quantumult.ps1
@@ -544,7 +544,7 @@ $generationTime = (Get-Date).ToUniversalTime().AddHours(8).ToString("yyyy-MM-dd 
 # 创建文本格式的字符串
 $textContent = @"
 # Title: AdBlock_Rule_For_Quantumult
-# Description: 适用于Loon的域名拦截规则集，每20分钟更新一次，确保即时同步上游减少误杀
+# Description: 适用于Quantumult的域名拦截规则集，每20分钟更新一次，确保即时同步上游减少误杀
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_Quantumult
 # LICENSE1：https://github.com/REIJI007/AdBlock_Rule_For_Quantumult/blob/main/LICENSE-GPL3.0
 # LICENSE2：https://github.com/REIJI007/AdBlock_Rule_For_Quantumult/blob/main/LICENSE-CC%20BY-NC-SA%204.0
